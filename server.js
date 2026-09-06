f26794b0f0d0c4d226d6e3950dba519b1e65aff0
FLEXHUBPREDICTIONS <jacquesmends9750@icloud.com>
2026-09-06 21:18:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
+  "https://flexhubpredictions.github.io",
+  "https://flexhubpredictions.com",
+  "https://www.flexhubpredictions.com",
   FRONTEND_URL
 ].filter(Boolean);
 
